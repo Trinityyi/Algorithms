47e6a56cb47b8353cbdaa15bf66add4a51cf73ac
Isabelle <maciv@hotmail.gr>
2019-12-10 19:53:49 +0200

    Add common sorting structure
    Refactor data to be static.
    Refactor constructor to Initialize.
    Add Reset method.
    Rename BubbleSort class to Sort.

---
+++ Sort/CommonBase.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+    public static partial class Sort 
+    {
+        static List<int> data = new List<int>();
+
+        static List<int> _data = new List<int>();
+
+        public static List<int> Data
+        {
+            get 
+            {
+                return data;
+            }
+        }
+        public static void Initialize(List<int> list) 
+        {
+            data = list;
+            _data = list;
+        }
+
+        public static void Reset() 
+        {
+            data = _data;
+        }
+    }
+}
